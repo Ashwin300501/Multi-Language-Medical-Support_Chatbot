d546755d9e1e005b5f736910518a64d5de5c50ac
Ashwin300501 <ashwinbharathi200157@gmail.com>
2025-11-09 21:05:48 +0530

    Changing requirements.txt and creating ipynb file and code, and adding data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="ashwin",
+    author_email="ashwinbharathi200157@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
